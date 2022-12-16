c99ad90d926a1f22162fca2bf69ce3b5e52b2535
Sajib <sajib.sohelrana21@gmail.com>
2022-12-16 09:54:46 +0600

    commit on 16.12.22

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
   return (
     <footer id="footer">
       <a href="" className="footer__logo">
-        JANNATUL
+        SOHEL RANA SAJIB
       </a>
       <ul className="permalinks">
         <li>
@@ -46,7 +46,7 @@ const Footer = () => {
         </a>
       </div>
       <div className="footer__copyright">
-        <small>&copy; Jannatul All rights reserved </small>
+        <small>&copy; sajib All rights reserved </small>
       </div>
     </footer>
   );
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
     <header>
       <div className="container header__container">
         <h5>Hello I'm</h5>
-        <h1>Jannatul Ferdous Roshni</h1>
+        <h1>Sohel Rana Sajib</h1>
         <h5 className="text-light">FullStack Developer</h5>
         <CTA />
         <HeaderSocials />
